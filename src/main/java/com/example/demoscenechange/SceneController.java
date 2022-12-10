e0565e0d58a99a066cf7670774687c7761ccca74
Andrzej <acieslik.enn@gmail.com>
2022-12-10 18:53:10 +0100

    Initial commit

---
+++ src/main/java/com/example/demoscenechange/SceneController.java
@@ -0,0 +1,2 @@
+package com.example.demoscenechange;public class SceneController {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.demoscenechange {
+    requires javafx.controls;
+    requires javafx.fxml;
+        requires javafx.web;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+            requires org.kordamp.ikonli.javafx;
+            requires org.kordamp.bootstrapfx.core;
+            requires eu.hansolo.tilesfx;
+            requires com.almasb.fxgl.all;
+    
+    opens com.example.demoscenechange to javafx.fxml;
+    exports com.example.demoscenechange;
+}
